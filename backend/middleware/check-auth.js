66e9bf480c4927e7acc92fa7990b4831d17f8c8b
Jilks Smith <jilks.smith@KE-C02D15D8ML7J.mshome.net>
2022-08-21 20:27:39 +0300

    Update check-auth.js

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,16 +3,16 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    console.log(token)
     try {
       const decodedToken = jwt.verify(token, process.env.JWT_KEY);
       req.userData = {
         email: decodedToken.email,
-        userId: decodedToken.userId
+        userId: decodedToken.userId,
+        userType: decodedToken.userType
       };
       next();
     } catch (error) {
-
+        res.status(401).json({ message:  'You are not authenticated!' });
     }
   } catch (error) {
     res.status(401).json({ message: 'You are not authenticated!' });
